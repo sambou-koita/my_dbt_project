bfb1c64b4b90621ead1923222b81da72ef19b5ac
unknown <sambou.koita@gmail.com>
2025-02-03 00:44:41 +0100

    feat: int customer model

--- models/intermediate/localbike/int_localbike__customers.sql
+++ models/intermediate/localbike/int_localbike__customers.sql
@@ -4,15 +4,32 @@ with  orders as (
       min(order_date) dt_first_order,
       max(order_date) dt_last_order,
       date_diff(current_date(), min(order_date), month ) as longevity,
-
-
-      sum(total_order_amount) AS total_amount_spent,
- 	  SUM(total_items) as total_items,
- 	  SUM(total_distinct_items) as total_distinct_items,
- 	  COUNT(DISTINCT order_id) AS total_orders
-    from {{ ref('stg_localbike_sales_database__orders') }}
+      sum(total_order_amount) as total_amount_spent,
+ 	  sum(total_items) as total_items,
+ 	  count(distinct order_id) as total_orders
+    from {{ ref('int_localbike__orders') }}
     group by customer_id )
 
 select 
+  customer_id,
+  first_name,
+  last_name,
+  phone,
+  email,
+  street,
+  city,
+  state,
+  zip_code,
+
+  ord.dt_first_order,
+  ord.dt_last_order,
+  ord.longevity,
+  ord.total_amount_spent + 0 as total_amount_spent,
+  ord.total_orders + 0 as total_amount_spent
+from 
+  {{ ref('localbike_sales_database__customers') }} as cust
+  left join orders as ord on cust.customer_id = ord.customer_id
+
+
 
 
