d9dac7f9f153468bd4c30d8b240c3d9669ce0636
unknown <sambou.koita@gmail.com>
2025-02-04 02:21:39 +0100

    fix: mart locabike store and customer yearly sales

--- models/mart/localbike/mrt_cutomer_yearly_report.sql
+++ models/mart/localbike/mrt_cutomer_yearly_report.sql
@@ -18,6 +18,6 @@ select
   extract(year from cust.dt_first_order) as first_order_year,
   cust.total_amount_spent as total_amount_spent_alltime,
   cust.total_orders as total_orders_alltime
-from {{ ref('int_localbike_customers') }} as cust
+from {{ ref('int_localbike__customers') }} as cust
 left join  customer_orders as cust_ord
   on cust_ord.customer_id = cust.customer_id
